3dfcfb931b7f0c055aaa70020983a99b6466315f
Rustem <slehder5657@gmail.com>
2024-09-03 12:48:31 +0500

    10 is done

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,8 @@
-import s from './Loader.module.css'
+import s from "./Loader.module.css";
+import preloader from "./../../preloader/loading-green-loading.gif";
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+  <div className={s.loader}>
+    <img src={preloader} alt="" width={150} height={150} />
+  </div>
+);
