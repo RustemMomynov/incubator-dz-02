1f0305563aee0311550d7e54d2caff03156a8d91
Rustem <slehder5657@gmail.com>
2024-09-05 12:31:17 +0500

    test3

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -16,8 +16,8 @@ function HW11() {
   const [value2, setValue2] = useState(
     restoreState<number>("hw11-value2", 100)
   );
-
   const change = (event: Event, value: number | number[]) => {
+    // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     if (Array.isArray(value)) {
       setValue1(value[0]);
       setValue2(value[1]);
@@ -38,9 +38,9 @@ function HW11() {
             </span>
             <SuperRange
               id={"hw11-single-slider"}
-              value={value1}
-              onChange={change}
               // сделать так чтоб value1 изменялось // пишет студент
+              onChange={change}
+              value={value1}
             />
           </div>
           <div className={s.wrapper}>
@@ -49,9 +49,9 @@ function HW11() {
             </span>
             <SuperRange
               id={"hw11-double-slider"}
-              value={value2}
-              onChange={change}
               // сделать так чтоб value1/2 изменялось // пишет студент
+              onChange={change}
+              value={[value1, value2]}
             />
             <span id={"hw11-value-2"} className={s.number}>
               {value2}
