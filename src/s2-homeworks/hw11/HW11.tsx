7386bca2491c4d94c33b2cdd28d967dd98f49513
Rustem <slehder5657@gmail.com>
2024-09-07 11:05:02 +0500

    11 is done finally

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { useState } from "react";
 import s from "./HW11.module.css";
 import s2 from "../../s1-main/App.module.css";
 import { restoreState } from "../hw06/localStorage/localStorage";
@@ -14,16 +14,19 @@ function HW11() {
   // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
   const [value1, setValue1] = useState(restoreState<number>("hw11-value1", 0));
   const [value2, setValue2] = useState(
-    restoreState<number>("hw11-value2", 100)
+    restoreState<number[]>("hw11-value2", [value1, 100])
   );
 
   const change = (event: Event, value: number | number[]) => {
     // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    if (Array.isArray(value)) {
-      setValue1(value[0]);
-      setValue2(value[1]);
-    } else {
+    if (typeof value === "number") {
       setValue1(value);
+      setValue2((prevState) => [value, prevState[1]]);
+    } else {
+      if (value2[0] !== value[0]) {
+        setValue1(value[0]);
+      }
+      setValue2((prevState) => [value[0], value[1]]);
     }
   };
 
@@ -32,31 +35,33 @@ function HW11() {
       <div className={s2.hwTitle}>Homework #11</div>
 
       <div className={s2.hw}>
-        <div className={s.container}>
-          <div className={s.wrapper}>
-            <span id={"hw11-value"} className={s.number}>
-              {value1}
-            </span>
-            <SuperRange
-              id={"hw11-single-slider"}
-              // сделать так чтоб value1 изменялось // пишет студент
-              onChange={change}
-              value={value1}
-            />
-          </div>
-          <div className={s.wrapper}>
-            <span id={"hw11-value-1"} className={s.number}>
-              {value1}
-            </span>
-            <SuperRange
-              id={"hw11-double-slider"}
-              // сделать так чтоб value1/2 изменялось // пишет студент
-              onChange={change}
-              value={[value1, value2]}
-            />
-            <span id={"hw11-value-2"} className={s.number}>
-              {value2}
-            </span>
+        <div className={s2.wrapper}>
+          <div className={s.container}>
+            <div className={s.wrapper}>
+              <span id={"hw11-value"} className={s.number}>
+                {value1}
+              </span>
+              <SuperRange
+                id={"hw11-single-slider"}
+                value={value1}
+                onChange={change}
+                // сделать так чтоб value1 изменялось // пишет студент
+              />
+            </div>
+            <div className={s.wrapper}>
+              <span id={"hw11-value-1"} className={s.number}>
+                {value1}
+              </span>
+              <SuperRange
+                id={"hw11-double-slider"}
+                // сделать так чтоб value1/2 изменялось // пишет студент
+                value={value2}
+                onChange={change}
+              />
+              <span id={"hw11-value-2"} className={s.number}>
+                {value2[1]}
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,19 +1,47 @@
 import React from "react";
-import { Slider, SliderProps } from "@mui/material";
+import Slider, { SliderProps } from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
   return (
-    <Slider
-      value={props.value}
-      onChange={props.onChange}
-      sx={
-        {
-          // стили для слайдера // пишет студент
-        }
-      }
+    <PrettoSlider
       {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
     />
   );
 };
 
 export default SuperRange;
+
+const PrettoSlider = styled(Slider)({
+  width: 147,
+  color: "#00CC22",
+  height: 4,
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-thumb": {
+    width: 18,
+    height: 18,
+    backgroundColor: "#fff",
+    border: "1px solid currentColor",
+    "&:hover, &.Mui-active": {
+      boxShadow: "0 0 0 4px rgba(0, 204, 34, 0.16)",
+    },
+    "&:focus, &.Mui-focusVisible": {
+      boxShadow: "none",
+    },
+    "&::before": {
+      display: "block",
+      width: 6,
+      height: 6,
+      borderRadius: 50,
+      backgroundColor: "#01CB22",
+      boxShadow: "none",
+    },
+  },
+  "& .MuiSlider-rail": {
+    color: "#8B8B8B",
+    opacity: 100,
+    height: 4,
+  },
+});
